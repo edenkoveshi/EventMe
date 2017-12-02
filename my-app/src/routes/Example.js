e303116d5cc86aeeaf97a6ba32c7dea8d61d914d
Ronnie Artzi <ronnie@getnexar.com>
2017-12-02 18:25:42 +0200

    no message

--- my-app/src/routes/Example.js
+++ my-app/src/routes/Example.js
@@ -1,13 +1,115 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
+import style from '../style.js';
+import {
+  Button,
+  Container,
+  Divider,
+  Grid,
+  Header,
+  Icon,
+  Image,
+  List,
+  Menu,
+  Segment,
+  Visibility,
+  Modal,
+  Item,
+  Dimmer,
+  TextArea,
+  Form
+} from 'semantic-ui-react'
 
-class Example extends Component {
+const FixedMenu = () => (
+  <Menu fixed='top' size='large'>
+    <Container style={style.container}>
+      <Menu.Item as='a' active>Home</Menu.Item>
+      <Menu.Item as='a'>Contact us</Menu.Item>
+      <Menu.Menu position='right'>
+      </Menu.Menu>
+    </Container>
+  </Menu>
+)
+
+export default class Example extends Component {
+  state = {}
+
+  hideFixedMenu = () => this.setState({ visible: false })
+  showFixedMenu = () => this.setState({ visible: true })
+
   render() {
+    const { visible } = this.state
+
     return (
       <div>
-        hi
+        { visible ? <FixedMenu /> : null }
+
+        <Visibility
+          onBottomPassed={this.showFixedMenu}
+          onBottomVisible={this.hideFixedMenu}
+          once={false}
+        >
+          <Segment
+            inverted
+            textAlign='center'
+            style={{ minHeight: 700, padding: '1em 0em'}}
+            vertical
+
+          >
+            <Container>
+              <Menu inverted pointing secondary size='large'>
+                <Menu.Item as='a' active>Home</Menu.Item>
+                <Menu.Item as='a'>Contact us</Menu.Item>
+                <Menu.Item position='right'>
+                </Menu.Item>
+              </Menu>
+            </Container>
+            <Container text>
+              <Header
+                as='h1'
+                content='When?'
+                inverted
+                style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }} />
+              <Header
+                as='h2'
+                content='Do whatever you want when you want to.'
+                inverted
+                style={{ fontSize: '1.7em', fontWeight: 'normal' }}/>
+            </Container>
+          </Segment>
+        </Visibility>
+
+        <Segment inverted vertical style={{ padding: '5em 0em' }}>
+          <Container>
+            <Grid divided inverted stackable>
+              <Grid.Row>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='About' />
+                  <List link inverted>
+                    <List.Item as='a'>Sitemap</List.Item>
+                    <List.Item as='a'>Contact Us</List.Item>
+                    <List.Item as='a'>Religious Ceremonies</List.Item>
+                    <List.Item as='a'>Gazebo Plans</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={3}>
+                  <Header inverted as='h4' content='Services' />
+                  <List link inverted>
+                    <List.Item as='a'>Banana Pre-Order</List.Item>
+                    <List.Item as='a'>DNA FAQ</List.Item>
+                    <List.Item as='a'>How To Access</List.Item>
+                    <List.Item as='a'>Favorite X-Men</List.Item>
+                  </List>
+                </Grid.Column>
+                <Grid.Column width={7}>
+                  <Header as='h4' inverted>Footer Header</Header>
+                  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+                </Grid.Column>
+              </Grid.Row>
+            </Grid>
+          </Container>
+        </Segment>
       </div>
-    );
+    )
   }
 }
 
-export default Example;
