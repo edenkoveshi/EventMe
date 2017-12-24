ca5ea8941707feb8bc9fd6633068446d68adaee8
Unknown <itay.vegh@gmail.com>
2017-12-24 20:52:26 +0200

    getEvent now works
    i.e. http://localhost:3000/event/11111

---
+++ my-app/src_back/public/js/myeventtable.js
@@ -0,0 +1,64 @@
+var myList = [
+  { "name": "abc", "age": 50, "nickname": "hey", "activity": "sport", "favorite bend": "beatles" },
+  { "age": "25", "hobby": "swimming" },
+  { "name": "xyz", "hobby": "programming"}, 
+  { "name": "xyz", "hobby": "programming"}, 
+  { "name": "xyz", "hobby": "programming"}, 
+  { "name": "xyz", "hobby": "programming"}, 
+  { "name": "xyz", "hobby": "programming"}, 
+  { "name": "xyz", "hobby": "programming"}, 
+  { "name": "xyz", "hobby": "programming"}
+];
+
+
+function boxChecked(t) {
+  if (t.is(':checked')) {
+    myList.push({ "child": "abc", "age": 50 });
+    $("#excelDataTable tr").remove();
+    buildMyEventsTable('#excelDataTable');
+  }
+}
+
+
+// Builds the HTML Table out of myList.
+function buildMyEventsTable(selector) {
+  $(selector).append($('<caption>'+"My Events"+'</caption>'));
+  var columns = addAllColumnHeaders(myList, selector);
+  for (var i = 0; i < myList.length; i++) {
+
+    var row$ = $('<tr>');
+
+    for (var colIndex = 0; colIndex < columns.length; colIndex++) {
+      var cellValue = myList[i][columns[colIndex]];
+      if (cellValue == null) cellValue = "";
+      if (columns[colIndex] == "name") {
+        row$.append($('<td>'+'<a href="event.html">'+cellValue+'</a>'+'</td>'+'</tr>'));
+      } else {
+        row$.append($('<td>'+cellValue+'</td>'+'</tr>'));
+      }
+    }
+    $(selector).append(row$);
+  }
+}
+
+
+// Adds a header row to the table and returns the set of columns.
+// Need to do union of keys from all records as some records may not contain
+// all records.
+function addAllColumnHeaders(myList, selector) {
+  var columnSet = [];
+  var headerTr$ = $('<tr/>');
+
+  for (var i = 0; i < myList.length; i++) {
+    var rowHash = myList[i];
+    for (var key in rowHash) {
+      if ($.inArray(key, columnSet) == -1) {
+        columnSet.push(key);
+        headerTr$.append($('<th/>').html(key));
+      }
+    }
+  }
+  $(selector).append(headerTr$);
+
+  return columnSet;
+}
--- my-app/src_back/server/index/indexRouter.js
+++ my-app/src_back/server/index/indexRouter.js
@@ -1,24 +1,43 @@
-var express = require('express');
-var router = express.Router();
-
+let express = require('express');
+let router = express.Router();
+const es = require('../mydb/eventService');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-    res.render('index', { title: 'indexRouter Working!' });
+    res.render('index', { title: 'eventMe index Working!' });
 });
 
+router.get('/event/:event_id', function(req, res){
+    let p = es.getEvent(req.params.event_id);
+    p.then((event) =>
+    {   console.log(event);
+        if (event != undefined) {
+            res.render('event', {
+                title: 'my event',
+                event_img: 'restaurant.jpeg',
+                event_time: event["time"],
+                event_place: event["location"],
+                event_type: event["type"],
+                event_desc: event["information"]
+            });
+        }
+        else{
+            res.render('404');
+        }
+    });
+});
 
 router.get('/hello', (req, res) => {
     res.send('Hello World!')
-})
+});
 
 
 router.get('/hello/:name/:lastName', (req, res) => {
-    console.log(req.params)
-    let name = req.params.name
-    let lastName = req.params.lastName
+    console.log(req.params);
+    let name = req.params.name;
+    let lastName = req.params.lastName;
     res.send('Hello ' + name + ' ' + lastName +'!')
-})
+});
 
 
 module.exports = router;
