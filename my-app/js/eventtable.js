f3df4281a3ae05e1bc9fd27f8678a102e15beecc
Ronnie Artzi <ronnie@getnexar.com>
2017-12-24 11:27:40 +0200

    no message

--- my-app/js/eventtable.js
+++ my-app/js/eventtable.js
@@ -1,27 +1,38 @@
 var myList = [
-  { "name": "abc", "age": 50, "nickname": "hey", "activity": "sport", "favorite bend": "beatles" },
-  { "age": "25", "hobby": "swimming" },
-  { "name": "xyz", "hobby": "programming"}, 
-  { "name": "xyz", "hobby": "programming"}, 
-  { "name": "xyz", "hobby": "programming"}, 
-  { "name": "xyz", "hobby": "programming"}, 
-  { "name": "xyz", "hobby": "programming"}, 
-  { "name": "xyz", "hobby": "programming"}, 
-  { "name": "xyz", "hobby": "programming"}
+  { "Category": "Food_Mexican", "Location": 345 },
+  { "Category": "Food_Hamburger", "Location": 645 },
+  { "Category": "Food_Italian", "Location": 375 },
+  { "Category": "Food_Israeli", "Location": 385 },
+  { "Category": "Food_Sushi", "Location": 349 },
+  { "Category": "Food_Chinese", "Location": 335 }
 ];
-
 
 function boxChecked(t) {
   if (t.is(':checked')) {
-    myList.push({ "child": "abc", "age": 50 });
+    var x = t.context.name;
+    var myCurrentList = [];
+    for (var listIndex = 0; listIndex < myList.length; listIndex++) {
+      if (myList[listIndex]["Category"] == x) {
+        myCurrentList.push(myList[listIndex]);
+      }
+    }
     $("#excelDataTable tr").remove();
-    buildEventsTable('#excelDataTable');
+    buildEventsTable('#excelDataTable', myCurrentList);
   }
 }
 
+function displaysearch() {
+  myList.push({ "child": "abc", "age": 50 });
+  $("#excelDataTable tr").remove();
+  buildEventsTable('#excelDataTable', myCurrentList);
+}
+
+function buildEventsTableMain(selector) {
+  buildEventsTable(selector, myList);
+}
 
 // Builds the HTML Table out of myList.
-function buildEventsTable(selector) {
+function buildEventsTable(selector, myList) {
   $(selector).append($('<caption>'+"Events"+'</caption>'));
   var columns = addAllColumnHeaders(myList, selector);
   for (var i = 0; i < myList.length; i++) {
@@ -31,7 +42,7 @@ function buildEventsTable(selector) {
     for (var colIndex = 0; colIndex < columns.length; colIndex++) {
       var cellValue = myList[i][columns[colIndex]];
       if (cellValue == null) cellValue = "";
-      if (columns[colIndex] == "name") {
+      if (columns[colIndex] == "Category") {
         row$.append($('<td>'+'<a href="event.html">'+cellValue+'</a>'+'</td>'+'</tr>'));
       } else {
         row$.append($('<td>'+cellValue+'</td>'+'</tr>'));
