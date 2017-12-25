77f02d0d669da351a96c7e11a8deee5c1f1d8123
Unknown <itay.vegh@gmail.com>
2017-12-25 21:43:54 +0200

    router supports "/" and "/event"

--- my-app/src_back/app.js
+++ my-app/src_back/app.js
@@ -1,18 +1,15 @@
-var express = require('express');
-var path = require('path');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var bodyParser = require('body-parser');
-var indexRouter = require('./server/index/indexRouter');
-var users = require('./server/users');
-var eventMe = require('./server/mydb/eventMeDbRouter')
-var app = express();
+let express = require('express');
+let path = require('path');
+let logger = require('morgan');
+let cookieParser = require('cookie-parser');
+let bodyParser = require('body-parser');
+let users = require('./server/users');
+let eventMe = require('./server/mydb/eventMeDbRouter');
+let app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
-
-
 
 
 app.use(logger('dev'));
@@ -22,13 +19,12 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
-app.use('/', indexRouter);
 app.use('/users', users);
 app.use('/eventMe', eventMe);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    var err = new Error('Not Found');
+    let err = new Error('Not Found');
     err.status = 404;
     next(err);
 });
