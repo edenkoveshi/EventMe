87b1b671069e356befa1f7da0c05b8a6374c1b97
BARAK\ברק <lightning144@gmail.com>
2018-01-03 12:20:03 +0200

    add location to the db and fix frontpage/newUser workflow

--- my-app/src_back/server/mydb/user.js
+++ my-app/src_back/server/mydb/user.js
@@ -2,7 +2,7 @@
 
 class  user
 {
-    constructor(fb_id, f_name, friends_list){
+    constructor(fb_id, f_name, friends_list, location){
         this.fb_id = fb_id
         this.f_name = f_name
         this.friends_list = friends_list
@@ -12,6 +12,7 @@ class  user
         this.own_public_events = []
         this.own_private_events = []
         this.created_events = 0
+        this.current_location = location
     }
 }
 
