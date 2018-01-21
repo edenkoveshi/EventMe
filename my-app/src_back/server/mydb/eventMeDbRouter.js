4d800db2f4706b80a8ece2e1d12794633394c697
BARAK\ברק <lightning144@gmail.com>
2018-01-21 19:18:21 +0200

    giving ownerid to client side

--- my-app/src_back/server/mydb/eventMeDbRouter.js
+++ my-app/src_back/server/mydb/eventMeDbRouter.js
@@ -47,7 +47,7 @@ router.get('/event/:event_id/:user_id', function (req, res) {
                 going_ids: (event["going_users"]==undefined ? [] : event["going_users"]),
                 pollArray: (event["pollArray"] == undefined ? [] : JSON.stringify(event["pollArray"])),
                 pollCounter: event["pollCounter"],
-                owner_id: event["ownerName"],
+                owner_id: event["ownerId"],
                 //pollQuestion: event["pollQuestion"],
             })
             ;
