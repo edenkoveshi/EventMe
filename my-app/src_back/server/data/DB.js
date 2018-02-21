137ecac1da5aa4b19b79bff88fba54be1ddc08e5
BARAK\ברק <lightning144@gmail.com>
2018-02-21 14:29:49 +0200

    working copy on local host

--- my-app/src_back/server/data/DB.js
+++ my-app/src_back/server/data/DB.js
@@ -8,12 +8,8 @@ class DB{
     }
     connectDB(){
         return new Promise((resolve, reject) =>{
-
-            var uri = "mongodb://eventme:Google123@cluster0-shard-00-00-fk6yv.mongodb.net:27017,cluster0-shard-00-01-fk6yv.mongodb.net:27017,cluster0-shard-00-02-fk6yv.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin";
-
-            //MongoClient.connect('mongodb://127.0.0.1:27017/mycloud') //returns a promise, or add callback - (err, database) =>  {
-
-            MongoClient.connect(uri).then(database => {
+            MongoClient.connect('mongodb://127.0.0.1:27017/mycloud') //returns a promise, or add callback - (err, database) =>  {
+                .then(database => {
                     this.db = database
                     resolve()
                 }).catch(err=> reject(err))
