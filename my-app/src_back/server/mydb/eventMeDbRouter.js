26f2fc4834166c124f952d972f6b268154cc4d80
Unknown <edenk3012@gmail.com>
2018-02-21 19:23:38 +0200

    Auto stash before merge of "master" and "origin/master"

--- my-app/src_back/server/mydb/eventMeDbRouter.js
+++ my-app/src_back/server/mydb/eventMeDbRouter.js
@@ -94,6 +94,7 @@ router.get('/event/:event_id/:user_id', function (req, res) {
                 pollArray: (event["pollArray"] == undefined ? [] : JSON.stringify(event["pollArray"])),
                 pollCounter: event["pollCounter"],
                 owner_id: event["ownerId"],
+                pool_results:event["pool_results"]
                 //pollQuestion: event["pollQuestion"],
             })
             ;
