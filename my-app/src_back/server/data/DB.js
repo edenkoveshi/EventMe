e41bde0ddf119dbba3e6119d81689dcaa7ae62e2
Unknown <itay.vegh@gmail.com>
2018-02-21 16:12:03 +0200

    mongo reads from env

--- my-app/src_back/server/data/DB.js
+++ my-app/src_back/server/data/DB.js
@@ -1,21 +1,25 @@
 const MongoClient = require('mongodb').MongoClient
+let mongoUrl = process.env.mongo_path == undefined ? 'mongodb://127.0.0.1:27017/mycloud' : process.env.mongo_path;
 
-class DB{
+class DB {
 
-    constructor(dbAddress){
+    constructor(dbAddress) {
         this.dbAddress = dbAddress
         this.db = undefined
     }
-    connectDB(){
-        return new Promise((resolve, reject) =>{
-            MongoClient.connect('mongodb://127.0.0.1:27017/mycloud') //returns a promise, or add callback - (err, database) =>  {
+
+    connectDB() {
+        return new Promise((resolve, reject) => {
+            console.log('connecting to DB at:'+mongoUrl);
+            MongoClient.connect(mongoUrl) //returns a promise, or add callback - (err, database) =>  {
                 .then(database => {
                     this.db = database
                     resolve()
-                }).catch(err=> reject(err))
+                }).catch(err => reject(err))
         })
     }
-    disconnectDB(){
+
+    disconnectDB() {
         mongoClient.close();
 
     }
